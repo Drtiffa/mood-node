381a62cf65d90ebd0e0e60cdddef0dfc29877c10
Tiffany Drouet <tiffanydrouet96@gmail.com>
2019-05-26 11:33:38 +0200

    add model and route for users login/register + init NODEMON

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+var users = require('./routes/users');
 const app = express();
 
+mongoose.connect('mongodb://localhost/mood-node', {useNewUrlParser: true});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.send('Home Page');
 });
+
+app.use('/users', users);
 
 app.listen( 3001, function() {
     console.log('Server started on port 3001...');
---
+++ routes/users.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var jwt = require('jsonwebtoken');
+var users = express.Router();
+var User = require("../models/user");
+
+// Sign in
+users.post('/signin', (req,res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  // permet de trouver user
+  User.find({ email, password }, (err,find_res) => {
+    if (err) {
+      res.json({ "resultType": "failure", "resultMessage": err });
+    } else if (find_res.length === 0) {
+      res.json({ "resultType": "failure", "resultMessage": "Wrong credentials." });
+    } else {
+      var token = jwt.sign({ email }, 'shhhhh'); // si bien connecter alors renvoi token
+      res.json({ "resultType": "success", "resultMessage": "You are successfully logged in.", "token": token });
+    }
+  })
+})
+
+// Sign up
+users.post('/signup', (req,res) => {
+    var newUser = new User(req.body)
+
+    newUser.save((err, user) => {
+        if (err) {
+        res.json({ "resultType": "failure", "resultMessage": err });
+        } else {
+        res.json({ "resultType": "success", "resultMessage": "Account successfully created." });
+        }
+    })
+})
+  
+  module.exports = users;
